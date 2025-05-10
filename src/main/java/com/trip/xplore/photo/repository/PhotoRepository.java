451d3a1f656abff5bc5baf6af6e345848240f2eb
YuSoeun <soeunsense7@gmail.com>
2025-05-11 00:12:59 +0900

    feat[#3]: JPA 엔티티 구조 정리
    - 복합 키를 사용하는 Like 엔티티에 대한 LikeId 클래스 최적화
    - 엔티티 패키지 구조 일관성 유지
    - photo에서 board로 이름 변경

--- src/main/java/com/trip/xplore/photo/repository/PhotoRepository.java
+++
@@ -1,8 +0,0 @@
-package com.trip.treaxure.photo.repository;
-
-import com.trip.treaxure.photo.entity.Photo;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PhotoRepository extends JpaRepository<Photo, Long> {
-    // TODO: Add custom queries if needed
-} 
